2933eb39a9fa6414bfab3c8747fd22f1454a8a56
Kuvam Bhardwaj <kuvam@oaktreeapps.com>
2023-06-13 11:02:19 +0530

    - fix test cases

--- tests/addconfig.test.js
+++ tests/addconfig.test.js
@@ -1,25 +1,30 @@
-const fs = require("fs-extra")
+const fs = require("fs-extra");
 const execAsync = require("./helpers/execAsync");
 
-const projectName = "admin_addconfig_test"
+const projectName = "admin_addconfig_test";
 
 beforeAll(async () => {
-    await execAsync(`npx ./ scaffold ${projectName}`)
-    process.chdir(`${projectName}`);
-    await execAsync(`npx ../ addconfig posts`);
-}, 50_000)
+  await execAsync(`npx ./ scaffold ${projectName}`);
+  process.chdir(`${projectName}`);
+  await execAsync(`npx ../ addconfig posts`);
+}, 50_000);
 
 afterAll(async () => {
-    await execAsync(`rm -rf ${projectName}`);
+  process.chdir("..");
+  await execAsync(`rm -rf ${projectName}`);
 });
 
-test.concurrent('Creating new config file', async () => {
+test.concurrent(
+  "Creating new config file",
+  async () => {
     const checks = [
-        fs.existsSync(`webapp`),
-        fs.existsSync(`server`),
-        fs.existsSync(`kitconfig`),
-        fs.existsSync(`kitconfig/resources/posts.cjs`),
-    ]
+      fs.existsSync(`webapp`),
+      fs.existsSync(`server`),
+      fs.existsSync(`kitconfig`),
+      fs.existsSync(`kitconfig/resources/posts.cjs`),
+    ];
 
-    expect(checks.filter(dirCheck => !dirCheck).length).toBe(0);
-}, 50_000);
+    expect(checks.filter((dirCheck) => !dirCheck).length).toBe(0);
+  },
+  50_000,
+);
